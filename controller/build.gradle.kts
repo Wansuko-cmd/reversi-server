fb22d2c195a68f4169d01484b47a91040803cae6
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-10-11 10:23:13 +0900

    [add] version catalogを導入

--- controller/build.gradle.kts
+++ controller/build.gradle.kts
@@ -1,11 +1,8 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     application
     kotlin("jvm") version "1.7.20"
-    id("io.ktor.plugin") version "2.1.2"
+    alias(libs.plugins.ktor)
 }
 
 group = "com.oucrc"
@@ -18,9 +15,6 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.bundles.ktor)
+    testImplementation(libs.bundles.test)
 }

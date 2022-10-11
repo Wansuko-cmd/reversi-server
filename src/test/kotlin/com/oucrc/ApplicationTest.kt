f8a76a8832564ac85631c9c7a36c8f1ed8e25954
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-10-11 09:46:53 +0900

    first commit

---
+++ src/test/kotlin/com/oucrc/ApplicationTest.kt
@@ -0,0 +1,25 @@
+package com.oucrc
+
+import com.oucrc.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}

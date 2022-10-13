5ad14e12882360494b3064581275db1a950460ff
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-10-14 08:49:01 +0900

    [add] DomainExceptionを追加

---
+++ domain/src/main/kotlin/DomainException.kt
@@ -0,0 +1,17 @@
+sealed class DomainException(
+    override val message: String,
+    override val cause: Throwable? = null,
+) : Exception() {
+    class NoSuchElementException(
+        override val message: String,
+    ) : DomainException(message)
+
+    class ValidationException(
+        override val message: String,
+    ) : DomainException(message)
+
+    class SystemException(
+        override val message: String,
+        override val cause: Throwable,
+    ) : DomainException(message, cause)
+}

7e6ac91f57d60619bb3c34964da6ded3953b5744
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-10-14 12:11:25 +0900

    [add] UserIndexのコードを追加

--- controller/src/main/kotlin/com/oucrc/routing/MainRoute.kt
+++ controller/src/main/kotlin/com/oucrc/routing/MainRoute.kt
@@ -3,6 +3,7 @@ package com.oucrc.routing
 import com.oucrc.routing.rooms.index.roomsIndexGet
 import com.oucrc.routing.rooms.show.roomsShowGet
 import com.oucrc.routing.rooms.show.roomsShowPost
+import com.oucrc.routing.users.index.usersIndexGet
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -19,6 +20,9 @@ fun Application.mainRoute() {
                 path = Routing.Room.Show.path,
                 params = Routing.Room.Show.roomId
             )
+        }
+        route(Routing.User.path) {
+            usersIndexGet(path = Routing.User.Index.path)
         }
         get("") { call.respond("Hello World") }
     }
--- controller/src/main/kotlin/com/oucrc/routing/users/index/UsersIndexGetRoute.kt
+++ controller/src/main/kotlin/com/oucrc/routing/users/index/UsersIndexGetRoute.kt
@@ -1,9 +1,27 @@
 package com.oucrc.routing.users.index
 
+import com.oucrc.serializable.ExceptionSerializable
+import com.oucrc.serializable.UserSerializable
+import com.wsr.result.consume
+import com.wsr.result.mapBoth
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 import user.GetUsersUseCase
 
 fun Route.usersIndexGet(path: String) {
     val getUsersUseCase by inject<GetUsersUseCase>()
+
+    get(path) {
+        getUsersUseCase()
+            .mapBoth(
+                success = { users -> users.map { UserSerializable.from(it) } },
+                failure = { ExceptionSerializable.from(it) }
+            )
+            .consume(
+                success = { users -> call.respond(users) },
+                failure = { (message, status) -> call.respond(status, message) }
+            )
+    }
 }
